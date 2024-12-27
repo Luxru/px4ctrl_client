33067baa74878e951e2317925de0ad39d8fe7fb1
luxu <lux@cqu.edu.cn>
2024-12-27 11:56:27 +0800

    add input

--- include/client.h
+++ include/client.h
@@ -2,6 +2,7 @@
 #include "datas.h"
 #include "types.h"
 
+#include <array>
 #include <cstdint>
 #include <map>
 #include <mutex>
@@ -43,5 +44,41 @@ namespace ui{
             std::vector<std::string> log_data;
             Px4Client& px4_client;
     };
+
+    inline std::array<double, 4> q_inv(const std::array<double, 4>& q){
+        // q = [w, x, y, z]
+        return {q[0], -q[1], -q[2], -q[3]};
+    }
+    
+    inline std::array<double, 4> q_mul(const std::array<double, 4>& q1, const std::array<double, 4>& q2){
+        //return q1*q2
+        // q = [w, x, y, z]
+        return {
+            q1[0]*q2[0] - q1[1]*q2[1] - q1[2]*q2[2] - q1[3]*q2[3],
+            q1[0]*q2[1] + q1[1]*q2[0] + q1[2]*q2[3] - q1[3]*q2[2],
+            q1[0]*q2[2] - q1[1]*q2[3] + q1[2]*q2[0] + q1[3]*q2[1],
+            q1[0]*q2[3] + q1[1]*q2[2] - q1[2]*q2[1] + q1[3]*q2[0]
+        };
+    }
+    
+    inline std::array<double, 3> q_rot(const std::array<double, 4>& q, const std::array<double, 3>& v){
+        //return q*v*q_inv
+        // q = [w, x, y, z]
+        std::array<double, 4> q_v = {0, v[0], v[1], v[2]};
+        std::array<double, 4> q_inv_v = q_mul(q, q_mul(q_v, q_inv(q)));
+        return {q_inv_v[1], q_inv_v[2], q_inv_v[3]};
+    }
+
+    inline double to_yaw(const std::array<double, 4>& q){
+        // q = [w, x, y, z]
+        // atan2(2.0f * (w * z + x * y), w * w + x * x - y * y - z * z);
+        return std::atan2(2.0f * (q[0] * q[3] + q[1] * q[2]), q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3]);
+    }
+
+    inline std::array<double, 4> from_yaw(double yaw){
+        // The quaternion representation of rotation is a variation on axis and angle. So if you rotate by r radians around axis x, y, z, then your quaternion q is:
+        // q = [cos(r/2), sin(r/2)x, sin(r/2)y, sin(r/2)z]
+        return {std::cos(yaw/2), 0, 0, std::sin(yaw/2)};
+    }
 }
 }
--- src/client.cpp
+++ src/client.cpp
@@ -2,6 +2,7 @@
 #include "datas.h"
 #include "types.h"
 
+#include <array>
 #include <cstdint>
 #include <imgui.h>
 #include <vector>
@@ -146,14 +147,134 @@ namespace  px4ctrl{
         void ImguiClient::render_window(){
             ImGuiIO& io = ImGui::GetIO(); (void)io;
             ImGui::Begin("UISC Px4 Client", NULL , ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize|ImGuiWindowFlags_NoTitleBar|ImGuiWindowFlags_NoCollapse|ImGuiWindowFlags_NoBackground);                          // Create a window called "Hello, world!" and append into it.
+            //Same line
+            ImGui::Text("UISC Px4 Client");
+            ImGui::SameLine();
+            ImGui::Text("v0.1");
+            ImGui::SameLine();
+            ImGui::TextDisabled("(?)");
+            if (ImGui::BeginItemTooltip())
+            {
+                ImGui::PushTextWrapPos(ImGui::GetFontSize() * 35.0f);
+                ImGui::TextUnformatted("Press W/S/A/D to move drone in XY plane\nPress R/F to move drone in Z axis\nPress Q/E to rotate drone\nPress Space to force hover\nPress J to force disarm");
+                ImGui::PopTextWrapPos();
+                ImGui::EndTooltip();
+            }
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+
+
+
+            //velocity in body frame
+            const double velocity_xy = 0.5; // TODO change velocity in config
+            const double velocity_z = 0.5;
+            const double velocity_yaw = 2;// ~120 degree/s
             for(const auto& elem:server_data_map){
                 const uint8_t id = elem.first;
                 const ServerPayload& drone = elem.second;
+                std::array<double,3> hover_pos = {drone.hover_pos[0],drone.hover_pos[1],drone.hover_pos[2]};
+                std::array<double, 4> hover_quat = {drone.hover_quat[0],drone.hover_quat[1],drone.hover_quat[2],drone.hover_quat[3]};
                 //Window
                 ImGui::PushID(fmt::format("Drone{}",id).c_str());
                 if(ImGui::BeginChild("DroneContainer",ImVec2(0,0),true)){
                     ImGui::Text("Drone ID: %d",id);
+                    // Red Text
+                    if(ImGui::IsWindowFocused(ImGuiFocusedFlags_ChildWindows)){
+                        // Green Text
+                        ImGui::TextColored(ImVec4(0,1,0,1), "Active");
+                        {
+                            double yaw = to_yaw(hover_quat);
+                            //first, get veclocity in world frame
+                            std::array<double,3> vel_body = {velocity_xy,velocity_xy,velocity_z};//body frame
+                            std::array<double, 4> quat = {drone.quat[0],drone.quat[1],drone.quat[2],drone.quat[3]};
+                            auto vel_world = q_rot(quat, vel_body);
+                            // TODO change velocity in config
+                            bool hover_pos_changed = false;
+                            const double delta_time_frame = io.DeltaTime;
+                            if(ImGui::IsKeyPressed(ImGuiKey_W)){
+                                spdlog::info("W key pressed");
+                                hover_pos[0] += vel_world[0]*delta_time_frame;
+                                hover_pos_changed = true;
+                            }
+                            if(ImGui::IsKeyPressed(ImGuiKey_S)){
+                                spdlog::info("S key pressed");
+                                hover_pos[0] -= vel_world[0]*delta_time_frame;
+                                hover_pos_changed = true;
+                            }
+                            if(ImGui::IsKeyPressed(ImGuiKey_A)){
+                                spdlog::info("A key pressed");
+                                hover_pos[1] += vel_world[1]*delta_time_frame;
+                                hover_pos_changed = true;
+                            }
+                            if(ImGui::IsKeyPressed(ImGuiKey_D)){
+                                spdlog::info("D key pressed");
+                                hover_pos[1] -= vel_world[1]*delta_time_frame;
+                                hover_pos_changed = true;
+                            }
+                            if(ImGui::IsKeyPressed(ImGuiKey_Q)){
+                                // yaw
+                                spdlog::info("Q key pressed");
+                                yaw += velocity_yaw*delta_time_frame;
+                                hover_pos_changed = true;
+                            }
+                            if(ImGui::IsKeyPressed(ImGuiKey_E)){
+                                // yaw
+                                spdlog::info("E key pressed");
+                                yaw -= velocity_yaw*delta_time_frame;
+                                hover_pos_changed = true;
+                            }
+                            if(ImGui::IsKeyPressed(ImGuiKey_R)){
+                                // z
+                                spdlog::info("R key pressed");
+                                hover_pos[2] += velocity_z*delta_time_frame;
+                                hover_pos_changed = true;
+                            }
+                            if (ImGui::IsKeyPressed(ImGuiKey_F)){
+                                // z
+                                spdlog::info("F key pressed");
+                                hover_pos[2] -= velocity_z*delta_time_frame;
+                                hover_pos_changed = true;
+                            }
+                            if(hover_pos_changed){
+                                //update hover pos
+                                ClientPayload payload;
+                                payload.command = ClientCommand::CHANGE_HOVER_POS;
+                                payload.timestamp = to_uint64(clock::now());
+                                payload.id = id;
+                                double data[7];
+                                data[0] = hover_pos[0];
+                                data[1] = hover_pos[1];
+                                data[2] = hover_pos[2];
+                                auto new_quat = from_yaw(yaw);
+                                data[3] = new_quat[0];
+                                data[4] = new_quat[1];
+                                data[5] = new_quat[2];
+                                data[6] = new_quat[3];
+                                std::memcpy(payload.data,data,sizeof(data));
+                                px4_client.pub_client(payload);
+                            }
+
+                            if (ImGui::IsKeyPressed(ImGuiKey_Space)){
+                                spdlog::info("Space key pressed");//force hover
+                                ClientPayload payload;
+                                payload.command = ClientCommand::FORCE_HOVER;
+                                payload.timestamp = to_uint64(clock::now());
+                                px4_client.pub_client(payload);
+                            }
+
+                            if (ImGui::IsKeyPressed(ImGuiKey_J)){
+                                spdlog::info("J key pressed");//Force Disarm
+                                ClientPayload payload;
+                                payload.command = ClientCommand::FORCE_DISARM;
+                                payload.timestamp = to_uint64(clock::now());
+                                px4_client.pub_client(payload);
+                            }
+
+                        }
+                    }else{
+                        // Red Text
+                        ImGui::TextColored(ImVec4(1,0,0,1), "Inactive");
+                    }
+
                     // Two columns
                     ImGui::Columns(2);
                     ImGui::Text("FSM Status: %s %s %s",Px4CtrlStateName[drone.fsm_state[0]],Px4CtrlStateName[drone.fsm_state[1]],Px4CtrlStateName[drone.fsm_state[2]]);
